2abb1fed2667fbf077b2705d43d57c928d78923a
parker <522515341@qq.com>
2025-04-11 10:01:48 +0800

    新建byteplus.vod的RPC服务

--- test/socket/run_test.go
+++ test/socket/run_test.go
@@ -11,15 +11,15 @@ import (
 )
 
 func TestUser1(t *testing.T) {
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDM0OTIxNDgsImlhdCI6MTc0Mjg4NzM0OCwidXNlcklkIjoxODk4OTIwMTMxNjY1OTQ0NTc2fQ.gCCdRQSqDY8O3bxjx6JlxJZy-rJ6NN4z_f3-I2LZMwc"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDQ5NDEyMjcsImlhdCI6MTc0NDMzNjQyNywidXNlcklkIjoxODk4OTIwMTMxNjY1OTQ0NTc2fQ.yUmHcfbyjakX574IUnHp-UlT2q3flbC_uCMjvuRZhHM"
 	s := NewServer(token)
-	s.RunSocket(1837056807609659392, 1898920131665944576)
+	s.RunSocket(0, 1898920131665944576)
 }
 
 func TestUser2(t *testing.T) {
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDM0OTI5MTksImlhdCI6MTc0Mjg4ODExOSwidXNlcklkIjoxODk4OTE5MjA2Nzc5OTY5NTM2fQ.pntwHSj83lz5x4OxCirpm5pmNELygFaoiEb5lHR1EhE"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDQ4ODUyMzcsImlhdCI6MTc0NDI4MDQzNywidXNlcklkIjoxOTA1MDgzMTQ3MTA5MzU5NjE2fQ.dneZ3d-7uqqQbZHcgrwEUs57xnEluApQUjVkIiVWQiM"
 	s := NewServer(token)
-	s.RunSocket(1837056807609659392, 1898919206779969536)
+	s.RunSocket(0, 1905083147109359616)
 }
 
 func TestWrite(t *testing.T) {
